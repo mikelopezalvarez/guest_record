e2fdd5dbd6b50a12b6699edef476e9c0cb77e857
Miguel Lopez <alvarez_webs@yahoo.com>
2017-07-07 13:21:22 -0400

    Hide Analytics Button in Menu
    menu.js
    --Comment the analytics button

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ function init_menu(e){
 	var menu = '<li id="menu_home" class="menu_link" rel="home"><a href="index.html" ><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a></li>' +
             '<li id="menu_events" class="menu_link" rel="events"><a href="events.html"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Events</a></li>' +
             '<li id="menu_lists" class="menu_link" rel="lists"><a href="lists.html"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Lists</a></li>' +
-            '<li id="menu_analytics" class="menu_link" rel="analytics"><a href="#"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Analytics</a></li>';
+            '<!--<li id="menu_analytics" class="menu_link" rel="analytics"><a href="#"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Analytics</a></li>-->';
 	//Append Menu
 	$("#menu").append(menu);
 	//Find Selected
